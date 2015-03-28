0b4500f87b864bae6838245effb3636576d05799
Simo-Pekka Koskinen <simo@turkuagileday.fi>
2015-03-28 17:10:04 +0200

    Modified create event-table evolution

--- conf/evolutions/default/2.sql
+++ conf/evolutions/default/2.sql
@@ -5,13 +5,9 @@
 create table EVENT (
     id bigint(20) NOT NULL AUTO_INCREMENT,
     name_ varchar(255) NOT NULL,
-    description_ varchar(255) NULL,
+    description varchar(255) NULL,
     date_of_event datetime NOT NULL,
     registration_start_date datetime NOT NULL,
     registration_end_date datetime NOT NULL,
     PRIMARY KEY (id)
 );
-
-# --- !Downs
-
-drop table EVENT;
