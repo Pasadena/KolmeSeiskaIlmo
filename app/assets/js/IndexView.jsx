c0ca9369815c9843ca4e980d70026015766b7f26
Simo-Pekka Koskinen <simo@turkuagileday.fi>
2015-06-05 14:45:19 +0300

    Modified front page to inlcude event list

---
+++ app/assets/js/IndexView.jsx
@@ -0,0 +1,60 @@
+var IndexViewComponent = React.createClass({
+    getInitialState: function() {
+        return {events: []};
+    },
+    componentDidMount: function() {
+        var eventRoute = '/admin/loadEvents';
+        $.ajax({
+            url: eventRoute,
+            dataType: 'json',
+            success: function(data) {
+                this.setState({events: data['events']});
+            }.bind(this),
+            error: function(xhr, status, err) {
+                console.error(status, err.toString());
+            }.bind(this)
+        });
+    },
+    render: function() {
+        var eventComponents = this.state.events.map(function(event) {
+            return (
+                <AvailableEventCard event={event} ref={event.id}/>
+            );
+        });
+        var eventList = !this.state.events.length ? <div>There are no events to display!</div> : eventComponents;
+        return (
+            <section>
+                <hgroup>
+                    <h1>Available events </h1>
+                </hgroup>
+                <div>
+                    {eventList}
+                </div>
+            </section>
+        );
+    }
+});
+
+var AvailableEventCard = React.createClass({
+    render: function() {
+        return (
+            <div className="card">
+                <div className="card-header">
+                    <span className="eventName">{this.props.event.name}</span>
+                </div>
+                <div className="card-body">
+                    <ul>
+                        <li>
+                            Event date: {this.props.event.dateOfEvent}
+                        </li>
+                        <li>
+                            Registration open: {this.props.event.registrationStartDate} - {this.props.event.registrationEndDate}
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        );
+    }
+});
+
+React.render(<IndexViewComponent />, document.getElementById('eventList'));
