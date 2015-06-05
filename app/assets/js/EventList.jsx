cdef092c0c390197210954b3b02af21c10f16eb8
Simo-Pekka Koskinen <simo@turkuagileday.fi>
2015-06-05 10:43:21 +0300

    REmoved old test file

--- app/assets/js/EventList.jsx
+++
@@ -1,9 +0,0 @@
-var MyComponent = React.createClass({
-  render: function() {
-    return (
-      <h1>Howdy</h1>
-    );
-  }
-});
-
-React.render(<MyComponent />, document.getElementById('events'));
--- public/javascripts/EventList.js
+++
@@ -1,9 +0,0 @@
-var MyComponent = React.createClass({displayName: "MyComponent",
-  render: function() {
-    return (
-      React.createElement("h1", null, "Howdy")
-    );
-  }
-});
-
-React.render(React.createElement(MyComponent, null), document.getElementById('events'));
