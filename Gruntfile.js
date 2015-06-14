e543916d0d4ad870ad671b176637afb7551c00b5
Simo-Pekka Koskinen <simo@turkuagileday.fi>
2015-06-14 11:14:38 +0300

    Deploy modifications

--- Gruntfile.js
+++ Gruntfile.js
@@ -36,6 +36,6 @@ module.exports = function(grunt) {
 
     grunt.registerTask('default', ['react']);
 
-    grunt.registerTask('heroku', 'react copy');
+    grunt.registerTask('heroku', ['react', 'copy']);
 
 };
