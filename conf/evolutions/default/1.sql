17128ed95270673e2ce65eb517c76cfdd51261a2
Simo-Pekka Koskinen <simo@turkuagileday.fi>
2015-09-06 20:28:33 +0300

    Changed evolutions to postgres-format

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -1,17 +1,27 @@
-# Cabins schema
+# --- Created by Slick DDL
+# To stop Slick DDL generation, remove this comment and start using Evolutions
 
 # --- !Ups
 
-create table CABIN (
-    id bigint(20) NOT NULL AUTO_INCREMENT,
-    name_ varchar(255) NOT NULL,
-    description_ varchar(255) NULL,
-    capacity numeric(18,0) NOT NULL,
-    price numeric(18,2) NOT NULL,
-    PRIMARY KEY (id)
-);
+create table "CABIN" ("ID" BIGSERIAL PRIMARY KEY,"NAME_" VARCHAR(254) NOT NULL,"DESCRIPTION_" VARCHAR(254) NOT NULL,"CAPACITY" INTEGER NOT NULL,"PRICE" DECIMAL(21,2) NOT NULL);
+create table "EVENT" ("ID" BIGSERIAL PRIMARY KEY,"NAME_" VARCHAR(254) NOT NULL,"DESCRIPTION" VARCHAR(254) NOT NULL,"DATE_OF_EVENT" DATE NOT NULL,"REGISTRATION_START_DATE" DATE NOT NULL,"REGISTRATION_END_DATE" DATE NOT NULL);
+create table "EVENT_CABIN" ("ID" BIGSERIAL PRIMARY KEY,"EVENT_ID" BIGINT,"CABIN_ID" BIGINT NOT NULL,"AMOUNT_" INTEGER NOT NULL);
+create table "REGISTERED_PERSONS" ("ID" BIGSERIAL PRIMARY KEY,"REGISTRATION_ID" BIGINT NOT NULL,"FIRST_NAME" VARCHAR(254) NOT NULL,"LAST_NAME" VARCHAR(254) NOT NULL,"EMAIL" VARCHAR(254) NOT NULL,"DATE_OF_BIRTH" VARCHAR(254) NOT NULL,"CLUB_NUMBER" VARCHAR(254) NOT NULL,"SELECTED_DINING" INTEGER NOT NULL,"CONTACT_PERSON" INTEGER NOT NULL);
+create table "REGISTRATION" ("ID" BIGSERIAL PRIMARY KEY,"CABIN_ID" BIGINT NOT NULL,"EVENT_ID" BIGINT NOT NULL,"TIMESTAMP_" TIMESTAMP);
+alter table "EVENT_CABIN" add constraint "CABIN_FK" foreign key("CABIN_ID") references "CABIN"("ID") on update NO ACTION on delete NO ACTION;
+alter table "EVENT_CABIN" add constraint "EVENT_FK" foreign key("EVENT_ID") references "EVENT"("ID") on update NO ACTION on delete NO ACTION;
+alter table "REGISTRATION" add constraint "CABIN_FK" foreign key("CABIN_ID") references "CABIN"("ID") on update NO ACTION on delete NO ACTION;
+alter table "REGISTRATION" add constraint "EVENT_FK" foreign key("EVENT_ID") references "EVENT"("ID") on update NO ACTION on delete NO ACTION;
 
-INSERT INTO CABIN(name_, description_, capacity, price) values ('A4', 'Kiva hytti', 4, 105.00);
-INSERT INTO CABIN(name_, description_, capacity, price) values ('B3', 'Paska hytti', 3, 78.00);
+# --- !Downs
 
+alter table "REGISTRATION" drop constraint "CABIN_FK";
+alter table "REGISTRATION" drop constraint "EVENT_FK";
+alter table "EVENT_CABIN" drop constraint "CABIN_FK";
+alter table "EVENT_CABIN" drop constraint "EVENT_FK";
+drop table "REGISTRATION";
+drop table "REGISTERED_PERSONS";
+drop table "EVENT_CABIN";
+drop table "EVENT";
+drop table "CABIN";
 
